332147c28e967fa50e54925c5ba3f7cb8e282869
cainba <cainbryce208@gmail.com>
2025-03-02 10:50:23 -0700

    Add TLS configuration and update build script in web server setup
    Signed-off-by: cainba <cainbryce208@gmail.com>

--- modules/web/types/webserver_types.ts
+++ modules/web/types/webserver_types.ts
@@ -1,7 +1,9 @@
 import type { ServeOptions } from "bun"
 import type { Middleware, RouteHandler } from "../types"
+import type { TlsOptions } from "tls"
 
 export type cbserver = ServeOptions & {
+    tls: TlsOptions
     middleware?: Middleware[]
     routers?: Record<string, RouteHandler>
     static?: Record<string, Response | string> // Optional: for serving static files
